cb9f408624b799228b7ba1a1d02f5b4d97b7b705
Patricia de Bruijn <patricia.de.bruijn@rws.nl>
2017-04-11 10:00:34 +0200

    Add springExample
    Signed-off-by: Patricia de Bruijn <patricia.de.bruijn@rws.nl>

---
+++ src/main/java/com/example/TestController.java
@@ -0,0 +1,18 @@
+//package com.example;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * Created by pivotal on 3/17/17.
+// */
+//@RestController
+//public class TestController {
+//
+//    @RequestMapping
+//    public ResponseEntity<String> home(){
+//        return ResponseEntity.ok("Hello");
+//    }
+//
+//}
